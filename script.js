8e645e59ff2150c1c7ee43c2df07e1c0d151e6cc
sidheshwarchoure <92964594+sidheshwarchoure@users.noreply.github.com>
2023-03-20 09:48:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 //your JS code here. If required.
-function manipulateData(array) {
+function manipulateData(input) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(array);
+      resolve(input);
     }, 3000);
   })
-  .then(array => {
-    const evens = array.filter(num => num % 2 === 0);
-    const odds = array.filter(num => num % 2 !== 0);
-    const filteredArray = [...evens, ...odds];
-    return filteredArray;
-  })
-  .then(array => {
-    const updatedArray = array.map(num => {
-      if (num % 2 === 0) {
-        return num * 2;
-      }
-      return num;
+  .then(numbers => {
+    const filteredNumbers = numbers.filter(number => number % 2 === 0);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(filteredNumbers);
+      }, 1000);
     });
-    return updatedArray;
-  });
-}
-
-manipulateData([1, 2, 3, 4])
+  })
+  .then(numbers => {
+    const multipliedNumbers = numbers.map(number => number * 2);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(multipliedNumbers);
+      }, 2000);
+    });
+  })
   .then(result => {
-    const outputDiv = document.getElementById("output");
-    outputDiv.textContent = result.join(", ");
+    const outputElement = document.getElementById("output");
+    outputElement.textContent = result.join(", ");
   })
   .catch(error => {
     console.error(error);
   });
+}
